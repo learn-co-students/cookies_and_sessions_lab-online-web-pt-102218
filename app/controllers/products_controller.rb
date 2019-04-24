2b191b4c83528cef7284c4bd5728cab33e7a7165
Johnathon Wood <jaywood128@gmail.com>
2019-04-24 18:56:32 -0400

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,8 +3,8 @@ class ProductsController < ApplicationController
     def index 
     end 
 
-    def add
-     cart << params[:product]
+    def add 
+      cart << params[:product]
      render :index
     end 
 end 
